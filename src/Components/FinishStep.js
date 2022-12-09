b458b8956522fb00ff6c2e1a9b91b7c9d34c9f3e
unusali1 <yali71507@gmail.com>
2022-12-10 02:27:31 +0600

    finishstep create

--- src/Components/FinishStep.js
+++ src/Components/FinishStep.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const FinishStep = () => {
-  return (
-    <div>FinishStep</div>
-  )
+    return (
+        <div className="App">
+            <div>
+                <h4>SUCCESS!</h4>
+                <h6> Your Have <br></br>Successfully <br></br>Signed Up.</h6>
+            </div>
+        </div>
+    )
 }
 
 export default FinishStep
