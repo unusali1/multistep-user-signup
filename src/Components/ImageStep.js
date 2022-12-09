409f26bcd8c533840afa77fae41d5ea50a3d2290
unusali1 <yali71507@gmail.com>
2022-12-10 02:15:44 +0600

    imagestep create

--- src/Components/ImageStep.js
+++ src/Components/ImageStep.js
@@ -2,7 +2,42 @@ import React from 'react'
 
 const ImageStep = () => {
   return (
-    <div>Image</div>
+    <div className='App'>
+
+
+      <div className="inputcontainer">
+        <label className="imagelabel">Upload Your Photo:</label>
+        <div>
+          <input
+
+            name="image"
+            type="file"
+            className="imageinput"
+          />
+        </div>
+      </div>
+
+      <div className="inputcontainer">
+        <label className="signaturelabel"> Upload Signature Photo:</label>
+        <div>
+          <input
+
+            name="signature"
+            type="file"
+            className="imageinput"
+          />
+        </div>
+      </div>
+
+      <div className='inputcontainer'>
+        <button className='next-btn' >Submit</button>
+      </div>
+      <div className='inputcontainer' >
+        <button className='previous-btn'  >Previous</button>
+      </div>
+
+
+    </div>
   )
 }
 
